98c6e606ea7637ecefcb8c8e9d66e548609d0ab6
QuaWoys <artutik253@gmail.com>
2026-03-20 10:20:36 +0200

    second commit

--- main.py
+++ main.py
@@ -1,1 +1,7 @@
-print("sveiki")
+import random
+maratons = []
+f = open('maratons.txt', 'w')
+for _ in range(365):
+    maratons.append(str(random.choices([0,1], [0.3, 0.7])[0]))
+f.write(','.join(maratons))
+f.close()
